c5a26b59538d6ff8ad14f923938a7c0c3d09b49d
robertgasparyan <robert.gasparyan@gmail.com>
2016-04-08 17:01:37 +0000

    Initial commit for test application

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,16 @@
+class WelcomeController < ApplicationController 
+
+     def home
+     
+     end
+     
+     def index
+          
+     end
+     
+     def about
+     
+     end
+     
+     
+end
